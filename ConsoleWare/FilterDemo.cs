ebeeeb204281b8a29237e09f1167ff165b2080ec
Walt Ritscher <waltr@scandiasoft.com>
2017-02-22 23:02:02 -0800

    Add starting code for filter demo

--- ConsoleWare/FilterDemo.cs
+++ ConsoleWare/FilterDemo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PublisherLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace ConsoleWare
 {
     class FilterDemo
     {
+
+		internal void FilterExample() {
+
+			// completion list UI has filters 
+			// to limit the list of suggested items.
+
+			var historyBook = new Book();
+		}
     }
 }
