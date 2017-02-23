6bfb9a035cb82d5eb9025c5c80a59440b93bbdc4
Walt Ritscher <waltr@scandiasoft.com>
2017-02-22 23:41:05 -0800

    Add RunToDemo class

---
+++ ConsoleWare/RunToDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWare {
+	class RunToDemo {
+
+		private void RunTo1() {
+			decimal monthlyPayment;
+			decimal rate = 5.6M;
+
+			// step into
+			monthlyPayment = Financial.CalculateMonthlyPayment(12, rate, 1200);
+
+			// step over
+			monthlyPayment = Financial.CalculateMonthlyPayment(24, rate, 2400);
+
+			// step out;
+			monthlyPayment = Financial.CalculateMonthlyPayment(12, rate, 1800);
+
+		}
+
+	}
+}
