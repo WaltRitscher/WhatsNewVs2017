47f557d5257aa2ae4e7ecf6c83e76979ab4cd559
Walt Ritscher <waltr@scandiasoft.com>
2017-02-22 23:07:46 -0800

    Add the StyleDemo class,

---
+++ ConsoleWare/StyleDemo.cs
@@ -0,0 +1,30 @@
+﻿using PublisherLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWare {
+	class StyleDemo {
+		internal void PreferVar() {
+			// prefer explicit type declare for built-in types
+			var flowerName = "Daffodil";
+			Console.WriteLine(flowerName);
+
+			// prefer var when variable type is apparent
+			var book = new Book();
+
+			var source = new BookDataSource();
+			Book bestSeller = source.GetBestSellingBook();
+		}
+
+		public void camelCaseMethodNames() {
+
+			if (true)
+				Console.WriteLine("true");
+
+
+		}
+	}
+}
