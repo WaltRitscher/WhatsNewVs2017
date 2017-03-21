9cd7f0667b654468c8ccd3e16b2326a10ae10a41
Walt Ritscher <waltritscher@outlook.com>
2017-03-21 14:38:40 -0700

    Add the empty project Add notes about the GitHub branches to avoid confusion about where to find the sample code.

---
+++ EmptyProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyProject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      // The example code for the demos 
+      // is in the GitHub branches.
+      // The master branch is empty.
+
+      //Branches
+      //  Chrome
+      //  CompletionListFilter
+      //  ExceptionHelper
+      //  FindAllRefs
+      //  GoToTool
+      //  etc.
+
+      // Learn more.  Watch the 'Visual Studio 2017: First Look' course on LinkedIn Learnin or Lynda.com
+
+      // https://www.linkedin.com/learning/visual-studio-2017-first-look?u=104
+
+      https://www.lynda.com/Visual-Studio-tutorials/Visual-Studio-2017-First-Look/577369-2.html
+    }
+  }
+}
